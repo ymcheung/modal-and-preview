209c0726dc36b21d4c4293801e1081a5a6a5a1f3
ymcheung <ymcheung.tw@me.com>
2022-06-01 11:29:42 +0800

    Add placeholder header styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,23 @@ import styles from '../styles/header.module.css';
 export default function Header() {
   return(
     <header className={styles.header}>
+      <nav>
+        <ul className={styles.navList}>
+          <li>
+            <a className={styles.navListLink} href="#">
+              <img src="/arrowLeft.svg" alt="上一頁" />
+            </a>
+          </li>
+          <li>
+            <a className={styles.navListLink} href="#">
+              <img src="/eye.svg" alt="預覽" />
+            </a>
+            <a className={`${styles.navListLink} ${styles.navListLinkMore}`} href="#">
+              <img src="/moreVertical.svg" alt="更多選項" />
+            </a>
+          </li>
+        </ul>
+      </nav>
       <h2 className={styles.title}>測驗封面</h2>
     </header>
   )
